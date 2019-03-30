99ade0c46b246b81899cce29592b849bbd48e688
bprlab <bprlab@bprlab-iMac-7.local>
2019-03-30 14:01:02 +0300

    spring boot

---
+++ src/main/java/tr/edu/medipol/yazilimaraclari/ders6SpringBoot/Ders6SpringBootApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yazilimaraclari.ders6SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ders6SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ders6SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/edu/medipol/yazilimaraclari/ders6SpringBoot/Ders6SpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package tr.edu.medipol.yazilimaraclari.ders6SpringBoot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ders6SpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
